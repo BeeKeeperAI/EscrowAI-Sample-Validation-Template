b72e83298b50f6a91a94bdb118d45d2ac446c2fa
Pavan Gupta <pg8p@virginia.edu>
2024-11-07 08:47:15 -0800

    Removing confusing default ENCLAVE_URL and Clarifying Encryption

--- example/covid-validation/app.py
+++ example/covid-validation/app.py
@@ -14,7 +14,7 @@ import EnclaveSDK
 from EnclaveSDK import File, Report, LogData
 
 # Use the ENCLAVE_URL environment variable to create an SDK configuration for the Sandbox
-configuration = EnclaveSDK.Configuration(os.getenv("ENCLAVE_URL", "https://localhost:5000"))
+configuration = EnclaveSDK.Configuration(os.getenv("ENCLAVE_URL"))
 # Use the SAS_URL environment variables to use the Data API in the Sandbox, otherwise default to None
 sas_url = os.getenv("SAS_URL", None) 
 if sas_url:
